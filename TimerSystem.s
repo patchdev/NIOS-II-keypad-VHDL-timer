dec828f4b5e62deed8a9fcddf3a724716a1ab878
Diego P <dpachecotorres@outlook.com>
2022-01-10 23:10:10 +0100

    Fixed identation

--- TimerSystem.s
+++ TimerSystem.s
@@ -27,258 +27,258 @@ br _start
 
 .org 0x20
 /*__IHR_______________________________________________________________________________*/
-	rdctl	et, ipending			/*Copy ipending to exception temporary*/
-	beq		et, zero, NO_INTERRUPT 	/*If not ipending, int is not expected by device*/
-	subi	ea, ea, 4				/*Positioning ea (except return addr)*/
-	subi	sp, sp, 4				/*Positioning sp (stack pointer)*/
-	stw		r23, 0(sp)				/*Store r23s value into SP direction in Memory*/
-	andi	r23, et, irq_keyboard 	/*Checks if is different to irq1*/
-	beq		r23, zero, END_HANDLER 	/*if true, exit IHR. Possible another method */
-	call 	IRQ1
+    rdctl	et, ipending			/*Copy ipending to exception temporary*/
+    beq		et, zero, NO_INTERRUPT 	/*If not ipending, int is not expected by device*/
+    subi	ea, ea, 4				/*Positioning ea (except return addr)*/
+    subi	sp, sp, 4				/*Positioning sp (stack pointer)*/
+    stw		r23, 0(sp)				/*Store r23s value into SP direction in Memory*/
+    andi	r23, et, irq_keyboard 	/*Checks if is different to irq1*/
+    beq		r23, zero, END_HANDLER 	/*if true, exit IHR. Possible another method */
+    call 	IRQ1
 
 END_HANDLER:
-	ldw		r23, 0(sp)  			/*Restore Stack*/
+    ldw		r23, 0(sp)  			/*Restore Stack*/
     addi	sp, sp, 4   			/*Stack again at last position*/
-	br		EXIT_IHR	
+    br		EXIT_IHR	
 
 
 NO_INTERRUPT:
-									/*No peripheral interrupt*/
+                                    /*No peripheral interrupt*/
 EXIT_IHR:
-	eret
-	
-	
+    eret
+    
+    
 /*__IRQ1__________________________________________________________________________*/
 
 /*__IRQ1: SAVE STACK______________________________________________*/	
 IRQ1:
-	subi	sp, sp, 4
-	stw		r2, 0(sp)
-	subi	sp, sp, 4
-	stw		r3, 0(sp)
-	subi	sp, sp, 4
-	stw		r4, 0(sp)
-	subi	sp, sp, 4
-	stw		r5, 0(sp)
-	subi	sp, sp, 4
-	stw		r6, 0(sp)
-	subi	sp, sp, 4
-	stw		r7, 0(sp)
-	subi	sp, sp, 4
-	stw		r8, 0(sp)
+    subi	sp, sp, 4
+    stw		r2, 0(sp)
+    subi	sp, sp, 4
+    stw		r3, 0(sp)
+    subi	sp, sp, 4
+    stw		r4, 0(sp)
+    subi	sp, sp, 4
+    stw		r5, 0(sp)
+    subi	sp, sp, 4
+    stw		r6, 0(sp)
+    subi	sp, sp, 4
+    stw		r7, 0(sp)
+    subi	sp, sp, 4
+    stw		r8, 0(sp)
 
 
 /*__ISR MAIN_________________________________________*/
 
 /*__GENERAL INIT_______________________________*/
-	movia	r2, addr_cols
-	movui	r3, 0x0F
-	beq		r2, r3, EXIT_ISR	/*Security Check*/
-	
+    movia	r2, addr_cols
+    movui	r3, 0x0F
+    beq		r2, r3, EXIT_ISR	/*Security Check*/
+    
 /*__IDENTIFYING KEY_____________________*/	
-	movui	r6, 0x4				/*r6 contains number of iterations.*/
-	movia	r4, RowsMask 		/*Has dinamic pointer.*/
+    movui	r6, 0x4				/*r6 contains number of iterations.*/
+    movia	r4, RowsMask 		/*Has dinamic pointer.*/
 POLLING:
-	ldbu	r5, 0(r4) 			/*Loads into r5 the current value of rowmask*/
-	
-	movia	r2, addr_rows
-	stbio	r5, 0(r2)			/*Writes current rowmask value into device*/
+    ldbu	r5, 0(r4) 			/*Loads into r5 the current value of rowmask*/
+    
+    movia	r2, addr_rows
+    stbio	r5, 0(r2)			/*Writes current rowmask value into device*/
 
-	
+    
 /*__TIMER DEBOUNCING____________________________*/
-	movia	r2, time_5ms		
-	movia 	r3, addr_cttimer
-	stwio	r2, 0(r3)			/*5ms waiting time*/
-	
-	movui	r2, 0x01
-	movia	r3, addr_loadstart
-	stbio	r2, 0(r3)			/*Load value to count*/
-	movui	r2, 0x02
-	stbio	r2, 0(r3)			/*Starts counting*/
-	
-	movia	r2, addr_timerout
-	movui	r3, 0x01			/*Watches timerout*/
+    movia	r2, time_5ms		
+    movia 	r3, addr_cttimer
+    stwio	r2, 0(r3)			/*5ms waiting time*/
+    
+    movui	r2, 0x01
+    movia	r3, addr_loadstart
+    stbio	r2, 0(r3)			/*Load value to count*/
+    movui	r2, 0x02
+    stbio	r2, 0(r3)			/*Starts counting*/
+    
+    movia	r2, addr_timerout
+    movui	r3, 0x01			/*Watches timerout*/
 DEBOUNCE:
-	ldbuio	r7, 0(r2)
-	bne		r7, r3, DEBOUNCE
-	
-	movia	r2, addr_loadstart
-	stbio	zero, 0(r2)			/*Clears start and load*/
-	
-	
+    ldbuio	r7, 0(r2)
+    bne		r7, r3, DEBOUNCE
+    
+    movia	r2, addr_loadstart
+    stbio	zero, 0(r2)			/*Clears start and load*/
+    
+    
 /*__IDENTIFYING KEY (CONT)_______________*/	
-	movia 	r2, addr_cols
-	ldwio	r3, 0(r2)				/*Read cols, again, stores in r3. RO*/
-	
-	movui	r2, 0x000F
-	bne		r3, r2, KEY_DETECTED	/*For this iteration, if cols are 0xF, then go to next iteration*/
+    movia 	r2, addr_cols
+    ldwio	r3, 0(r2)				/*Read cols, again, stores in r3. RO*/
+    
+    movui	r2, 0x000F
+    bne		r3, r2, KEY_DETECTED	/*For this iteration, if cols are 0xF, then go to next iteration*/
 
-	
+    
 /*From here continues if not key is detected*/
-	addi	r4, r4, 1			/*Next pointer position for rowsmask*/
-	subi	r6, r6, 1				
-	beq		r6, zero, EXIT_ISR	/*If is the last iteration. Gets out*/
-	br	POLLING					/*Next Poll*/
-	
+    addi	r4, r4, 1			/*Next pointer position for rowsmask*/
+    subi	r6, r6, 1				
+    beq		r6, zero, EXIT_ISR	/*If is the last iteration. Gets out*/
+    br	POLLING					/*Next Poll*/
+    
 
 /*__SAVING KEY____________________________*/
 /*At this point r5 has the rows value, r3 has the cols values */
 KEY_DETECTED:
-	roli	r5, r5, 4 			/*Converts a given value 0x0N to 0xN0*/
-	or		r5, r5, r3			/*r5 stores the values in this order: rows + cols*/
-	
-	
+    roli	r5, r5, 4 			/*Converts a given value 0x0N to 0xN0*/
+    or		r5, r5, r3			/*r5 stores the values in this order: rows + cols*/
+    
+    
 /*__SHOW IN 7 SEG_______________________________________*/
 /*__7SEG INIT___________________________________________*/
-	movia	r4, MaskBits		/*Dynamic pointer for the bits mask*/
-	movia	r6, HexbyteDot		/*Dynamic pointer for the hex values*/
-	
+    movia	r4, MaskBits		/*Dynamic pointer for the bits mask*/
+    movia	r6, HexbyteDot		/*Dynamic pointer for the hex values*/
+    
 LOOP_7SEG:	
-	ldbu	r7, 0(r4)			/*Loads maskbit current value. RO*/
-	ldbu	r8, 0(r6)			/*Loads hexbyte current value. RO */
-	
-	addi	r4, r4, 1
-	addi	r6, r6, 1			/*Next pointer position for maskbits and hexbytedot*/
-	
-	bne     r5, r7, LOOP_7SEG	/*Compares key(rows+cols) with the keycheck register*/
-	
+    ldbu	r7, 0(r4)			/*Loads maskbit current value. RO*/
+    ldbu	r8, 0(r6)			/*Loads hexbyte current value. RO */
+    
+    addi	r4, r4, 1
+    addi	r6, r6, 1			/*Next pointer position for maskbits and hexbytedot*/
+    
+    bne     r5, r7, LOOP_7SEG	/*Compares key(rows+cols) with the keycheck register*/
+    
 /*From here, key is identified*/
-	movia 	r2, addr_7segsB
-	ldbuio	r3, 0(r2)
-	movui	r2, 0xFF
-	beq	r3, r2, RIGHTSEG	
-		
+    movia 	r2, addr_7segsB
+    ldbuio	r3, 0(r2)
+    movui	r2, 0xFF
+    beq	r3, r2, RIGHTSEG	
+        
 LEFTSEG:
-	movia	r2, addr_7segsA
-	stbio	r8, 0(r2)			/*Loads hexbyte value into device*/
-	movia	r2, addr_7segsB
-	movui	r3, 0xFF
-	stbio	r3, 0(r2)			/*Shutdown the other 7seg*/
-	br 	EXIT_ISR	
-		
+    movia	r2, addr_7segsA
+    stbio	r8, 0(r2)			/*Loads hexbyte value into device*/
+    movia	r2, addr_7segsB
+    movui	r3, 0xFF
+    stbio	r3, 0(r2)			/*Shutdown the other 7seg*/
+    br 	EXIT_ISR	
+        
 RIGHTSEG:
-	movia	r2, addr_7segsB
-	stbio	r8, 0(r2)			/*Loads hexbyte value into device*/
-	movia	r2, addr_7segsA
-	movui	r3, 0xFF
-	stbio	r3, 0(r2)			/*Shutdown the other 7seg*/
+    movia	r2, addr_7segsB
+    stbio	r8, 0(r2)			/*Loads hexbyte value into device*/
+    movia	r2, addr_7segsA
+    movui	r3, 0xFF
+    stbio	r3, 0(r2)			/*Shutdown the other 7seg*/
 
-	
+    
 EXIT_ISR:
 
-	movia 	r2, addr_cols
-	movui	r3, 0x0F
-	stbio	r3, 12(r2)			/*Clears edgecapture*/
+    movia 	r2, addr_cols
+    movui	r3, 0x0F
+    stbio	r3, 12(r2)			/*Clears edgecapture*/
 
 /*__TIMER EXIT DELAY____________________________*/
-	movia	r2, 0xC350			/*1 ms WAIT*/
-	movia 	r3, addr_cttimer
-	stwio	r2, 0(r3)			/*Sets value to count*/
-	
-	movui	r2, 0x01
-	movia	r3, addr_loadstart
-	stbio	r2, 0(r3)			/*Load value to count*/
-	movui	r2, 0x02
-	stbio	r2, 0(r3)			/*Starts counting*/
-	
-	movia	r2, addr_timerout
-	movui	r3, 0x01			/*Watches timerout*/
+    movia	r2, 0xC350			/*1 ms WAIT*/
+    movia 	r3, addr_cttimer
+    stwio	r2, 0(r3)			/*Sets value to count*/
+    
+    movui	r2, 0x01
+    movia	r3, addr_loadstart
+    stbio	r2, 0(r3)			/*Load value to count*/
+    movui	r2, 0x02
+    stbio	r2, 0(r3)			/*Starts counting*/
+    
+    movia	r2, addr_timerout
+    movui	r3, 0x01			/*Watches timerout*/
 EXIT_DELAY:
-	ldbuio	r7, 0(r2)
-	bne	r7, r3, EXIT_DELAY
-	
-	movia	r2, addr_loadstart
-	stbio	zero, 0(r2)			/*Clears start and load*/
-	
-	
-	movia	r2, addr_rows
-	stbio	zero, 0(r2)		    /*Rows cleared to any key for next reading*/
-	
-	
+    ldbuio	r7, 0(r2)
+    bne	r7, r3, EXIT_DELAY
+    
+    movia	r2, addr_loadstart
+    stbio	zero, 0(r2)			/*Clears start and load*/
+    
+    
+    movia	r2, addr_rows
+    stbio	zero, 0(r2)		    /*Rows cleared to any key for next reading*/
+    
+    
 /*___________________ISR RESTORE STACK______________________*/	
-	ldw		r8, 0(sp)
-	addi	sp, sp, 4
-	ldw		r7, 0(sp)
-	addi	sp, sp, 4
-	ldw		r6, 0(sp)
-	addi	sp, sp, 4
-	ldw		r5, 0(sp)
-	addi	sp, sp, 4
-	ldw		r4, 0(sp)
-	addi	sp, sp, 4
-	ldw		r3, 0(sp)
-	addi	sp, sp, 4
-	ldw		r2, 0(sp)
-	addi	sp, sp, 4
-	
+    ldw		r8, 0(sp)
+    addi	sp, sp, 4
+    ldw		r7, 0(sp)
+    addi	sp, sp, 4
+    ldw		r6, 0(sp)
+    addi	sp, sp, 4
+    ldw		r5, 0(sp)
+    addi	sp, sp, 4
+    ldw		r4, 0(sp)
+    addi	sp, sp, 4
+    ldw		r3, 0(sp)
+    addi	sp, sp, 4
+    ldw		r2, 0(sp)
+    addi	sp, sp, 4
+    
 ret	
-	
+    
 .global _start
 _start:	
 
-	
+    
 /*__SETUP_______________________________________*/
-	movia	sp, addr_sp			/*Init stack pointer last position*/
-	movui	r2, 0x01
-	wrctl	status, r2			/*Activates interruptions PIE*/
-	movui	r2, irq_keyboard
-	wrctl	ienable, r2			/*Activates Keyboard Int*/
-	movia	r2, addr_cols
-	movui	r3, 0x0F
-	stbio	r3, 8(r2)			/*Interrupt mask setup*/
-	movui	r2, 0xFF
-	movia	r3, addr_7segsA
-	stbio	r2, 0(r3)
-	movia	r3, addr_7segsB
-	stbio	r2, 0(r3)			/*Turns off both 7segs*/
-	movia	r2, addr_rows
-	stbio	zero, 0(r2)			/*Rows at 0, means waiting for any key*/	
-	movia	r2, addr_leds
-	stbio	zero, 0(r2)			/*Turns off the leds*/
-		
+    movia	sp, addr_sp			/*Init stack pointer last position*/
+    movui	r2, 0x01
+    wrctl	status, r2			/*Activates interruptions PIE*/
+    movui	r2, irq_keyboard
+    wrctl	ienable, r2			/*Activates Keyboard Int*/
+    movia	r2, addr_cols
+    movui	r3, 0x0F
+    stbio	r3, 8(r2)			/*Interrupt mask setup*/
+    movui	r2, 0xFF
+    movia	r3, addr_7segsA
+    stbio	r2, 0(r3)
+    movia	r3, addr_7segsB
+    stbio	r2, 0(r3)			/*Turns off both 7segs*/
+    movia	r2, addr_rows
+    stbio	zero, 0(r2)			/*Rows at 0, means waiting for any key*/	
+    movia	r2, addr_leds
+    stbio	zero, 0(r2)			/*Turns off the leds*/
+        
 /*___________________MAIN______________________*/
 
 
 /*__BLINK DELAY____________________________*/
 BLINK_LOOP:
-	movia	r3, time_500ms			
-	movia 	r4, addr_cttimer
-	stwio	r3, 0(r4)			/*500ms waiting time*/
-	
-	movui	r3, 0x01
-	movia	r4, addr_loadstart
-	stbio	r3, 0(r4)			/*Load value to count*/
-	movui	r3, 0x02
-	stbio	r3, 0(r4)			/*Starts counting*/
-	stbio	zero, 0(r4)			/*clear start*/
-	
-	movia	r3, addr_timerout
-	movui	r4, 0x01			/*Watches timerout r3.*/
-	
+    movia	r3, time_500ms			
+    movia 	r4, addr_cttimer
+    stwio	r3, 0(r4)			/*500ms waiting time*/
+    
+    movui	r3, 0x01
+    movia	r4, addr_loadstart
+    stbio	r3, 0(r4)			/*Load value to count*/
+    movui	r3, 0x02
+    stbio	r3, 0(r4)			/*Starts counting*/
+    stbio	zero, 0(r4)			/*clear start*/
+    
+    movia	r3, addr_timerout
+    movui	r4, 0x01			/*Watches timerout r3.*/
+    
 WAIT:
-	ldbuio	r7, 0(r3)
-	bne		r7, r4, WAIT		/*Checks timerout r7.*/
-	
-	xori	r5, r5, 0xFF
-	stbio	r5, 0(r2)			/*Toggle Leds on-off*/
-	
-	br BLINK_LOOP
+    ldbuio	r7, 0(r3)
+    bne		r7, r4, WAIT		/*Checks timerout r7.*/
+    
+    xori	r5, r5, 0xFF
+    stbio	r5, 0(r2)			/*Toggle Leds on-off*/
+    
+    br BLINK_LOOP
 
-	
+    
 /*__________________SOURCES_________________________*/	
 .data
 
 /*The mask is applied to the rows (output) for identifying the pressed key*/
 RowsMask:
-	.byte 0x0E, 0x0D, 0x0B, 0x07
-	
+    .byte 0x0E, 0x0D, 0x0B, 0x07
+    
 
 /* Mask bits from 0 to D and symbols asterisk and hashtag. Maybe wrong order*/
 MaskBits:
-	.byte 	0x7D,0xEE,0xED,0xEB,0xDE,0xDD,0xDB,0xBE,0xBD,0xBB,0xE7, 0xD7, 0xB7, 0X77, 0x7E, 0x7B
-	/*      0    1    2    3    4    5    6    7    8    9    A     B     C     D     asterisk hastag  */
-	
+    .byte 	0x7D,0xEE,0xED,0xEB,0xDE,0xDD,0xDB,0xBE,0xBD,0xBB,0xE7, 0xD7, 0xB7, 0X77, 0x7E, 0x7B
+    /*      0    1    2    3    4    5    6    7    8    9    A     B     C     D     asterisk hastag  */
+    
 /* Hex bytes for direct display on 7seg*/	
 HexbyteDot:
-	.byte	0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90, 0x88, 0x83, 0xC6, 0xA1, 0xB9, 0x81
+    .byte	0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90, 0x88, 0x83, 0xC6, 0xA1, 0xB9, 0x81
 
--- top.vhd
+++ top.vhd
@@ -4,31 +4,31 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY top IS 
-	PORT(
-		CLOCK_50	:	IN	STD_LOGIC;
-		RESET		:	IN	STD_LOGIC;
-		KEYB_IN		:	IN	STD_LOGIC_VECTOR(3 DOWNTO 0);
-		KEYB_OUT	:	OUT	STD_LOGIC_VECTOR(3 DOWNTO 0);
-		SEGMENT_0 	: 	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0);
-		SEGMENT_1 	: 	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0);
-		LEDS		: 	OUT	STD_LOGIC_VECTOR(6 DOWNTO 0);
-		TIMER_OUT	:	OUT	STD_LOGIC;
-		TIMER_LOAD	:	OUT	STD_LOGIC;
-		TIMER_START	:	OUT STD_LOGIC
-	);
+    PORT(
+        CLOCK_50	:	IN	STD_LOGIC;
+        RESET		:	IN	STD_LOGIC;
+        KEYB_IN		:	IN	STD_LOGIC_VECTOR(3 DOWNTO 0);
+        KEYB_OUT	:	OUT	STD_LOGIC_VECTOR(3 DOWNTO 0);
+        SEGMENT_0 	: 	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0);
+        SEGMENT_1 	: 	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0);
+        LEDS		: 	OUT	STD_LOGIC_VECTOR(6 DOWNTO 0);
+        TIMER_OUT	:	OUT	STD_LOGIC;
+        TIMER_LOAD	:	OUT	STD_LOGIC;
+        TIMER_START	:	OUT STD_LOGIC
+    );
 END top;
 
 ARCHITECTURE keyboard_system OF top IS
-	
-	SIGNAL	START_C_INT		:	STD_LOGIC;
-	SIGNAL	LOAD_INT		:	STD_LOGIC;
-	SIGNAL	CT_INT			:	STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL	TIMER_OUT_INT	:	STD_LOGIC;
-	SIGNAL	AUX_RESET		:	STD_LOGIC := '1';
-	
-	
+    
+    SIGNAL	START_C_INT		:	STD_LOGIC;
+    SIGNAL	LOAD_INT		:	STD_LOGIC;
+    SIGNAL	CT_INT			:	STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL	TIMER_OUT_INT	:	STD_LOGIC;
+    SIGNAL	AUX_RESET		:	STD_LOGIC := '1';
+    
+    
 
-	COMPONENT Nios is
+    COMPONENT Nios is
         PORT (
             clk_clk                 :	IN  STD_LOGIC                     := 'X';             -- clk
             reset_reset_n           :	IN  STD_LOGIC                     := 'X';             -- reset_n
@@ -41,54 +41,54 @@ ARCHITECTURE keyboard_system OF top IS
             timer_load_start_export :	OUT STD_LOGIC_VECTOR(1 DOWNTO 0)  :=(others => '0');  -- export
             timer_ct_export         :	OUT STD_LOGIC_VECTOR(31 DOWNTO 0) := (others => '0')  -- export
         );
-    	END COMPONENT Nios;
-	
-	COMPONENT watchdog IS
-		PORT(
-			CLK			:	IN	STD_LOGIC;
-			RESET		:	IN	STD_LOGIC;
-			START_C		:	IN	STD_LOGIC;
-			LOAD		:	IN	STD_LOGIC;
-			CTC			:	IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
-			TIMER_OUT 	:	OUT STD_LOGIC
-		);
-	END COMPONENT watchdog;	
-	
-		
-		
-	BEGIN
-	
-	    mySystem : Nios
-		PORT MAP (
-		    clk_clk                 	=> CLOCK_50,        -- clk.clk
-		    reset_reset_n           	=> RESET,           -- reset.reset_n
-		    keyboard_cols_export    	=> KEYB_IN,    		-- keyboard_cols.export
-		    keyboard_rows_export    	=> KEYB_OUT,    	-- keyboard_rows.export
-		    id7segments_1_export    	=> SEGMENT_1,    	-- id7segments_1.export
-		    id7segments_0_export    	=> SEGMENT_0,    	-- id7segments_0.export
-		    leds_export             	=> LEDS,            -- leds.export
-		    timer_out_export        	=> TIMER_OUT_INT,   -- timer_out.export
-		    timer_load_start_export(0) 	=> LOAD_INT, 		-- timer_load_start.export
-		    timer_load_start_export(1) 	=> START_C_INT,		-- timer_load_start.export
-		    timer_ct_export         	=> CT_INT       	-- timer_ct.export
-		);
-	
-		myTimer	: watchdog
-			PORT MAP (
-				CLK			=> CLOCK_50,
-				RESET		=> RESET,
-				START_C		=> START_C_INT, 
-				LOAD		=> LOAD_INT,
-				CTC			=> CT_INT,
-				TIMER_OUT 	=> TIMER_OUT_INT
-			);
-		
-		--Timer out, load and start assignments
-			TIMER_OUT 	<= TIMER_OUT_INT;
-			TIMER_LOAD 	<= LOAD_INT;
-			TIMER_START <= START_C_INT;
-		--Reset inversion
-		-- AUX_RESET <= RESET;
+        END COMPONENT Nios;
+    
+    COMPONENT watchdog IS
+        PORT(
+            CLK			:	IN	STD_LOGIC;
+            RESET		:	IN	STD_LOGIC;
+            START_C		:	IN	STD_LOGIC;
+            LOAD		:	IN	STD_LOGIC;
+            CTC			:	IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
+            TIMER_OUT 	:	OUT STD_LOGIC
+        );
+    END COMPONENT watchdog;	
+    
+        
+        
+    BEGIN
+    
+        mySystem : Nios
+        PORT MAP (
+            clk_clk                 	=> CLOCK_50,        -- clk.clk
+            reset_reset_n           	=> RESET,           -- reset.reset_n
+            keyboard_cols_export    	=> KEYB_IN,    		-- keyboard_cols.export
+            keyboard_rows_export    	=> KEYB_OUT,    	-- keyboard_rows.export
+            id7segments_1_export    	=> SEGMENT_1,    	-- id7segments_1.export
+            id7segments_0_export    	=> SEGMENT_0,    	-- id7segments_0.export
+            leds_export             	=> LEDS,            -- leds.export
+            timer_out_export        	=> TIMER_OUT_INT,   -- timer_out.export
+            timer_load_start_export(0) 	=> LOAD_INT, 		-- timer_load_start.export
+            timer_load_start_export(1) 	=> START_C_INT,		-- timer_load_start.export
+            timer_ct_export         	=> CT_INT       	-- timer_ct.export
+        );
+    
+        myTimer	: watchdog
+            PORT MAP (
+                CLK			=> CLOCK_50,
+                RESET		=> RESET,
+                START_C		=> START_C_INT, 
+                LOAD		=> LOAD_INT,
+                CTC			=> CT_INT,
+                TIMER_OUT 	=> TIMER_OUT_INT
+            );
+        
+        --Timer out, load and start assignments
+            TIMER_OUT 	<= TIMER_OUT_INT;
+            TIMER_LOAD 	<= LOAD_INT;
+            TIMER_START <= START_C_INT;
+        --Reset inversion
+        -- AUX_RESET <= RESET;
 END keyboard_system;
 
 
